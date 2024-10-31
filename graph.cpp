3c106b1288fe29233f443273e974b81ff1f4bcc2
Tuan Vy <hoatong2411@gmail.com>
2024-10-31 17:41:35 +0700

    Update Graph

--- graph.cpp
+++ graph.cpp
@@ -1,144 +1,160 @@
 #include "graph.h"
 
-// AdjencyList
-void AdjencyList::ParseLine(char* line, List*& lst) {
-	char* context = nullptr;
-	char* p = strtok_s(line, " ", &context);
-	int count = atoi(p), num;
-	while ((p = strtok_s(NULL, " ", &context)) != NULL) {
-		num = atoi(p);
-		insertTail(*lst, num);
-	}
+AdjencyList::AdjencyList(const char* filename) {
+	LoadGraph(filename);
 }
 
-AdjencyList::AdjencyList(const char* filename) {
-	// Load graph
+void AdjencyList::LoadGraph(const char* filename) {
 	FILE* fp = NULL;
 	errno_t err = fopen_s(&fp, filename, "r");
 	if (err != 0) {
 		exit(1);
 	}
+
 	// Read nVer
 	fscanf_s(fp, "%d\n", &nVer);
 	lst.resize(nVer);
+	for (int i = 0; i < nVer; i++) {
+		lst[i] = new List();
+	}
+
+	// Parse input
 	char line[200];
 	int i = 0;
 	while (fgets(line, sizeof(line), fp)) {
-		lst[i] = new List();
-		ParseLine(line, lst[i]);
+		char* context = nullptr;
+		// Extract number of vertexs connect to this vertex
+		char* p = strtok_s(line, " ", &context);
+		while ((p = strtok_s(NULL, " ", &context)) != NULL) {
+			lst[i]->push_back(atoi(p));
+		}
 		i++;
 	}
 	fclose(fp);
 }
 
 // Copy constructor
-AdjencyList::AdjencyList(AdjencyList& org) {
-	nVer = org.nVer;
+AdjencyList::AdjencyList(AdjencyList& org) : nVer(org.nVer){
 	lst.resize(nVer);
 	for (int i = 0; i < nVer; i++) {
-		lst[i] = new List();
-		Node* p = org.lst[i]->head;
-		while (p != NULL) {
-			insertTail(*lst[i], p->data);
-			p = p->pNext;
-		}
+		lst[i] = new List(*org.lst[i]);
 	}
 }
 
 AdjencyList::~AdjencyList() {
 	for (int i = 0; i < nVer; i++) {
-		freeList(*lst[i]);
+		delete lst[i];
 	}
 }
 
 void AdjencyList::printGraph() {
 	printf("nVer: %d\n", nVer);
 	for (int i = 0; i < nVer; i++) {
-		printf("Point ");
-		Node* p = lst[i]->head;
-		while (p) {
+		printf("Point %d: ", i);
+		Node* p = lst[i]->get_head();
+		while (p != NULL) {
 			printf("%d ", p->data);
 			p = p->pNext;
 		}
-		printf("\t Number of vertexs: %d\n", lst[i]->szList);
+		printf("\t\tNumber of vertexs: %d\n", lst[i]->get_size());
 	}
+	printf("\n");
+}
+
+List* AdjencyList::operator[](int index) {
+	if (index < 0 || index >= nVer) {
+		return NULL;
+	}
+	return lst[index];
+}
+
+// Euler constructor
+Euler::Euler(AdjencyList* g) : Graph(g) {
+	(*g).printGraph();
+	printf("Nhap dinh bat dau: ");
+	scanf_s("%d", &startVer);
+	EulerType = checkEulerGraph(startVer);
+}
+
+bool Euler::hasCircuit() const {
+	return (EulerType == 2);
+}
+
+bool Euler::hasPath() const {
+	return (EulerType == 1);
 }
 
 // Remove and Add Edge function
-void Euler::removeEdge(int u, int v){
+Edge Euler::removeEdge(int u, int v){
 	if (u < 0 || v < 0 || u >= Graph->nVer || v >= Graph->nVer) {
-		return;
+		return { -1, -1 ,0 , 0};
 	}
 
+	Edge e = { u, v , 0, 0 };
 	// Remove ver v from adjencyList of u
-	Node* p1 = Graph->lst[u]->head;
+	Node* p1 = (*Graph)[u]->get_head();
 	while (p1 != NULL) {
+		e.v_pos++;
 		if (p1->data == v) {
 			p1->data = -1;
 			break;
 		}
 		p1 = p1->pNext;
 	}
-	Graph->lst[u]->szList--;
+	(*Graph)[u]->decreaseSize();
 
 	// Remove ver u from adjencyList of v
-	Node* p2 = Graph->lst[v]->head;
+	Node* p2 = (*Graph)[v]->get_head();
 	while (p2 != NULL) {
+		e.u_pos++;
 		if (p2->data == u) {
 			p2->data = -1;
 			break;
 		}
 		p2 = p2->pNext;
 	}
-	Graph->lst[v]->szList--;
+	(*Graph)[v]->decreaseSize();
+
+	return e;
 }
 
-void Euler::addEdge(int u, int v) {
+void Euler::addEdge(int u, int v, Edge e) {
 	if (u < 0 || v < 0 || u >= Graph->nVer || v >= Graph->nVer) {
 		return;
 	}
 
 	// Add ver v to adjencyList of u
-	Node* p1 = Graph->lst[u]->head;
+	Node* p1 = (*Graph)[u]->get_head();
 	while (p1 != NULL) {
-		if (p1->data == -1) {
+		e.v_pos--;
+		if (e.v_pos == 0 && p1->data == -1) {
 			p1->data = v;
 			break;
 		}
 		p1 = p1->pNext;
 	}
-	Graph->lst[u]->szList++;
+	(*Graph)[u]->increaseSize();
 
 	// Add ver u to adjencyList of v
-	Node* p2 = Graph->lst[v]->head;
+	Node* p2 = (*Graph)[v]->get_head();
 	while (p2 != NULL) {
-		if (p2->data == -1) {
+		e.u_pos--;
+		if (e.u_pos == 0 && p2->data == -1) {
 			p2->data = u;
 			break;
 		}
 		p2 = p2->pNext;
 	}
-	Graph->lst[v]->szList++;
+	(*Graph)[v]->increaseSize();
 }
 
 // DFS
-void Euler::DFS_visit(int ver, bool* label) {
-	label[ver] = true;
-	Node* p = Graph->lst[ver]->head;
-	while (p != NULL) {
-		if (label[p->data] == false) {
-			DFS_visit(p->data, label);
-		}
-		p = p->pNext;
-	}
-}
-
 bool Euler::isConnected() {
 	bool* label = new bool[Graph->nVer];
 	memset(label, false, Graph->nVer * sizeof(bool));
 	int startVer = -1; 
 	for (int i = 0; i < Graph->nVer; i++) {
-		if (Graph->lst[i]->szList != 0) {
+		if ((*Graph)[i]->get_size() != 0) {
 			startVer = i;
 			break;
 		}
@@ -148,9 +164,9 @@ bool Euler::isConnected() {
 		return true;
 	}
 	bool connect = true;
-	DFS_visit(startVer, label);
+	DFS_visit(Graph, startVer, label);
 	for (int j = 0; j < Graph->nVer; j++) {
-		if (label[j] == false && Graph->lst[j]->szList != 0) {
+		if (label[j] == false && (*Graph)[j]->get_size() != 0) {
 			connect = false;
 			break;
 		}
@@ -167,7 +183,7 @@ int Euler::DFS_Count(int ver, bool* label) {
 
 	int count = 1;
 	label[ver] = true;
-	Node* p = Graph->lst[ver]->head;
+	Node* p = (*Graph)[ver]->get_head();
 	while (p != NULL) {
 		if (p->data >= 0 && label[p->data] == false) {
 			count += DFS_Count(p->data, label);
@@ -177,17 +193,17 @@ int Euler::DFS_Count(int ver, bool* label) {
 	return count;
 }
 
-int Euler::isEuler(int startVer) {
+int Euler::checkEulerGraph(int startVer) {
 	if (isConnected() == false) {
 		return 0;
 	}
 	int odd = 0;
 	for (int i = 0; i < Graph->nVer; i++) {
-		if (Graph->lst[i]->szList % 2 == 1) {
+		if ((*Graph)[i]->get_size() % 2 == 1) {
 			odd++;
 		}
 	}
-	if (odd > 2 || (odd == 2 && Graph->lst[startVer]->szList%2 == 0)) {
+	if (odd > 2 || (odd == 2 && (*Graph)[startVer]->get_size() %2 == 0)) {
 		return 0;
 	}
 	else {
@@ -203,27 +219,27 @@ bool Euler::isBrigde(int u, int v) {
 	
 	// Count vertex reachable from u before remove edge {u,v}
 	memset(label, false, Graph->nVer * sizeof(bool));
-	removeEdge(u, v);
+	Edge e = removeEdge(u, v);
 	int countAfter = DFS_Count(u, label);
 
 	// Restore {u,v}
-	addEdge(u, v);
+	addEdge(u, v, e);
 	delete[]label;
 
 	return (countBefore > countAfter);
 }
 
 void Euler::Euler_Rec(int ver) {
-	if (ver < 0 || ver >= Graph->nVer || Graph->lst[ver]->szList == 0) {
+	if (ver < 0 || ver >= Graph->nVer || (*Graph)[ver]->get_size() == 0) {
 		// printf("End at ver %d\n", ver);
 		return;
 	}
-	Node* p = Graph->lst[ver]->head;
+	Node* p = (*Graph)[ver]->get_head();
 	while (p != NULL) {
 		// printf("DEBUG VER %d at val: %d\n", ver, p->data);
 		if (p->data >= 0) {
 			int u = ver, v = p->data;
-			if (Graph->lst[u]->szList == 1 || !isBrigde(u, v)) {
+			if ((*Graph)[u]->get_size() == 1 || !isBrigde(u, v)) {
 				printf("%d -> %d \n",u, v);
 				removeEdge(u, v);
 				Euler_Rec(v);
@@ -234,37 +250,116 @@ void Euler::Euler_Rec(int ver) {
 }
 
 void Euler::Euler_Print() {
-	printf("Nhap dinh bat dau: ");
-	int startVer;
-	scanf_s("%d", &startVer);
-	int isEuler_val = isEuler(startVer);
-	if (isEuler_val > 0) {
-		if (isEuler_val == 1) {
+	if (hasCircuit() || hasPath()) {
+		if (hasPath()) {
 			printf("Duong di Euler:\n");
 		}
 		else {
 			printf("Chu trinh Euler:\n");
 		}
 		Euler_Rec(startVer);
+		printf("\n");
 	}
 	else {
 		printf("Do thi khong co duong di hay chu trinh Euler\n");
 	}
-	return;
 }
 
 Hamilton::Hamilton(AdjencyList* g) : Graph(g) {
-	circuit = new int[Graph->nVer];
-	memset(circuit, 0, Graph->nVer * sizeof(int));
+	circuit.resize(0);
+	path.resize(0);
+	curPath.resize(Graph->nVer, 0);
+
+	// Run algorithm
+	(*Graph).printGraph();
+	run();
+	if (!circuit.empty()) {
+		HamType = 2;
+	}
+	else if (!path.empty()) {
+		HamType = 1;
+	}
+	else {
+		HamType = 0;
+	}
 }
 
 Hamilton::~Hamilton() {
-	delete[]circuit;
-	circuit = NULL;
+	
+}
+
+bool Hamilton::hasCircuit() const {
+	return (HamType == 2);
+}
+bool Hamilton::hasPath() const {
+	return (HamType == 1);
+}
+
+Edge Hamilton::removeEdge(int u, int v) {
+	if (u < 0 || v < 0 || u >= Graph->nVer || v >= Graph->nVer) {
+		return { -1, -1 ,0 , 0 };;
+	}
+
+	Edge e = { u, v , -1, -1 };
+	// Remove ver v from adjencyList of u
+	Node* p1 = (*Graph)[u]->get_head();
+	while (p1 != NULL) {
+		e.v_pos++;
+		if (p1->data == v) {
+			p1->data = -1;
+			break;
+		}
+		p1 = p1->pNext;
+	}
+	(*Graph)[u]->decreaseSize();
+
+	// Remove ver u from adjencyList of v
+	Node* p2 = (*Graph)[v]->get_head();
+	while (p2 != NULL) {
+		e.u_pos++;
+		if (p2->data == u) {
+			p2->data = -1;
+			break;
+		}
+		p2 = p2->pNext;
+	}
+	(*Graph)[v]->decreaseSize();
+
+	return e;
+}
+
+void Hamilton::addEdge(int u, int v, Edge e) {
+	if (u < 0 || v < 0 || u >= Graph->nVer || v >= Graph->nVer) {
+		return;
+	}
+
+	// Add ver v to adjencyList of u
+	Node* p1 = (*Graph)[u]->get_head();
+	while (p1 != NULL) {
+		if (e.v_pos == 0 && p1->data == -1) {
+			p1->data = v;
+			break;
+		}
+		e.v_pos--;
+		p1 = p1->pNext;
+	}
+	(*Graph)[u]->decreaseSize();
+
+	// Add ver u to adjencyList of v
+	Node* p2 = (*Graph)[v]->get_head();
+	while (p2 != NULL) {
+		if (e.u_pos == 0 && p2->data == -1) {
+			p2->data = u;
+			break;
+		}
+		e.u_pos--;
+		p2 = p2->pNext;
+	}
+	(*Graph)[v]->decreaseSize();
 }
 
 bool Hamilton::isEdge(int u, int v) {
-	Node* p = Graph->lst[u]->head;
+	Node* p = (*Graph)[u]->get_head();
 	while (p != NULL) {
 		if (p->data == v) {
 			return true;
@@ -274,231 +369,130 @@ bool Hamilton::isEdge(int u, int v) {
 	return false;
 }
 
-void Hamilton::run(int ver) {
-	// Algorithm: https://www.youtube.com/watch?v=dQr4wZCiJJ4
+bool Hamilton::isConnected() {
+	if (Graph->nVer == 1) {
+		return true;
+	}
 
-	printf("Hamilton run at %d", ver);
-	do {
-		NextVertex(ver);
-		if (circuit[ver] == -1) {
-			// No vertexs found
+	bool* label = new bool[Graph->nVer];
+	memset(label, false, Graph->nVer * sizeof(bool));
+	for (int i = 0; i < Graph->nVer; i++) {
+		if ((*Graph)[i]->get_size() == 0) {
+			delete[]label;
+			return false;
+		}
+	}
+	bool connect = true;
+	DFS_visit(Graph, 0, label);
+	for (int j = 0; j < Graph->nVer; j++) {
+		if (label[j] == false) {
+			connect = false;
+			break;
+		}
+	}
+	delete[]label;
+	return connect;
+}
+
+void Hamilton::run() {
+	printf("Nhap dinh bat dau: ");
+	int startVer;
+	scanf_s("%d", &startVer);
+
+	if (!isConnected()) {
+		printf("Do thi khong lien thong nen khong the tim duoc duong di/chu trinh Hamilton\n");
+		return;
+	}
+
+	// Run Algorithm
+	int pos = 1;
+	curPath[0] = startVer;
+	
+	Node* p = (*Graph)[curPath[0]]->get_head();
+	while (p != NULL) {
+		// printf("%d -> %d\n", curPath[0], p->data);
+		Hamiton_Rec(pos, p->data);
+		p = p->pNext;
+	}
+}
+
+void Hamilton::Hamiton_Rec(int pos, int val) {
+	curPath[pos] = val;
+	// printf("Hamilton run at %d\tVal = %d\n", pos, val);
+	if (!isEdge(curPath[pos - 1], curPath[pos])) {
+		return;
+	}
+
+	Edge e = removeEdge(curPath[pos - 1], curPath[pos]);
+	// (*Graph).printGraph();
+
+	for (int i = 0; i < pos; i++) {
+		if (curPath[i] == curPath[pos]) {
+			// A vertex duplicate in circuit array
+			// printf("Duplicate with pos %d, val = %d\n", i, curPath[i]);
+			addEdge(curPath[pos - 1], curPath[pos], e);
 			return;
 		}
-		if (ver == Graph->nVer - 1) {
-			// last vertexs forms a cycle with first vertexs
-			if (isEdge(circuit[0], circuit[ver])) {
-				printf("%d ", circuit[0]);
-				for (int i = 1; i < Graph->nVer; i++) {
-					printf("-> %d ", circuit[i]);
-				}
-				printf("\n");
-			}
+	}
+
+	if (pos == Graph->nVer - 1) {
+		// Case forms a circuit
+		if (isEdge(curPath[0], curPath[pos])) {
+			circuit.push_back(curPath);
 		}
 		else {
-			printf("\tValue: %d\n", circuit[ver]);
-			run(ver + 1);
+			path.push_back(curPath);
 		}
-	} while (true);
+		addEdge(curPath[pos - 1], curPath[pos], e);
+		return;
+	}
+
+	// Recursion
+	Node* p = (*Graph)[curPath[pos]]->get_head();
+	while (p != NULL) {
+		if (p->data >= 0) {
+			//printf("%d -> %d\n", curPath[pos], p->data);
+			Hamiton_Rec(pos + 1, p->data);
+		}
+		p = p->pNext;
+	}
+	addEdge(curPath[pos - 1], curPath[pos], e);
 }
 
-void Hamilton::NextVertex(int ver) {
-	do {
-		// Start vertex is 0, so need to minus 1 after taking modular
-		circuit[ver] = (circuit[ver] + 1) % Graph->nVer;
-
-		// No vertexs found
-		if (circuit[ver] == -1) {
-			return;
-		}
-
-		// Vertex 0 have no previous vertex in circuit array
-		if (ver == 0) {
-			return;
-		}
-		if (isEdge(circuit[ver - 1], circuit[ver])) {
-			int i = 0;
-			for (; i < ver; i++) {
-				if (circuit[i] == circuit[ver]) {
-					break;
-				}
+void Hamilton::Ham_print() {
+	if (hasCircuit()) {
+		printf("Chu trinh Hamilton xuat phat tu dinh %d:\n", curPath[0]);
+		// Print all circuit
+		for (int i = 0; i < circuit.size(); i++) {
+			printf("%d ", circuit[i][0]);
+			for (int j = 1; j < Graph->nVer; j++) {
+				printf("-> %d ", circuit[i][j]);
 			}
-			if (i == ver) {
-				if (ver < Graph->nVer - 1 || ((ver == Graph->nVer - 1) && isEdge(circuit[0], circuit[ver]))){
-					return;
-				}
-			}
+			printf("-> %d\n", circuit[i][0]);
 		}
-	} while (true);
+		return;
+	}
+	if (hasPath()) {
+		printf("Duong di Hamilton xuat phat tu dinh %d:\n", curPath[0]);
+		// Print all circuit
+		for (int i = 0; i < circuit.size(); i++) {
+			printf("%d ", circuit[i][0]);
+			for (int j = 1; j < Graph->nVer; j++) {
+				printf("-> %d ", circuit[i][j]);
+			}
+			printf("\n");
+		}
+		return;
+	}
 }
 
-// AjdencyMatrix
-AdjencyMatrix::AdjencyMatrix(const char* filename) {
-		FILE* fp = NULL;
-		errno_t err = fopen_s(&fp, filename, "r");
-		if (err != 0) {
-			exit(1);
+void DFS_visit(AdjencyList* g, int ver, bool* label) {
+	label[ver] = true;
+	Node* p = (*g)[ver]->get_head();
+	while (p != NULL) {
+		if (label[p->data] == false) {
+			DFS_visit(g, p->data, label);
 		}
-		fscanf_s(fp, "%d\n", &nVer);
-		data.resize(nVer);
-		for (int i = 0; i < nVer; i++) {
-			data[i].resize(nVer);
-			for (int j = 0; j < nVer; j++) {
-				fscanf_s(fp, "%d", &data[i][j]);
-			}
-		}
-		fclose(fp);
-}
-
-void AdjencyMatrix::printGraph() {
-	for (int i = 0; i < nVer; i++) {
-		for (int j = 0; j < nVer; j++) {
-			printf("%3d", data[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-// ShortestPath
-void shortestPath::Dijkstra(AdjencyMatrix& a, int source) {
-	// Label
-	bool* lb = new bool[a.nVer];
-	memset(lb, 1, a.nVer);
-	// Length
-	int* len = new int[a.nVer];
-	for (int i = 0; i < a.nVer; i++) {
-		len[i] = INT32_MAX;
-	}
-	len[source] = 0;
-
-	// Prev
-	int* prev = new int[a.nVer];
-	for (int i = 0; i < a.nVer; i++) {
-		prev[i] = -1;
-	}
-	prev[source] = 0;
-
-	// Run
-	int n = a.nVer;
-	bool find = true;
-	while (n > 0 && find) {
-		// Find min L[i]
-		int minL = INT32_MAX, ver = -1;
-		for (int i = 0; i < a.nVer; i++) {
-			if (lb[i] == true && len[i] < minL) {
-				minL = len[i];
-				ver = i;
-			}
-		}
-		// Check
-		if (ver == -1) {
-			find = false;
-			break;
-		}
-		// Update L[j]
-		for (int i = 0; i < a.nVer; i++) {
-			if (a.data[ver][i] != 0 && len[i] > (len[ver] + a.data[ver][i])) {
-				prev[i] = ver;
-				len[i] = len[ver] + a.data[ver][i];
-			}
-		}
-		lb[ver] = false;
-		n--;
-	}
-
-	// Print result
-	for (int i = 0; i < a.nVer; i++) {
-		if (lb[i] == false && i != source) {
-			printf("Duong di tu dinh %d den dinh %d: %d <- ", source + 1, i + 1, i + 1);
-			int ver = prev[i];
-			while (ver != source) {
-				printf("%d <- ", ver + 1);
-				ver = prev[ver];
-			}
-			printf("%d\n", ver + 1);
-		}
-	}
-
-	printf("\n");
-	for (int i = 0; i < a.nVer; i++) {
-		if (i != source) {
-			printf("Ver %d - Len: %d\n", i, len[i]);
-		}
-	}
-	printf("\n");
-	delete[]lb;
-	delete[]len;
-	delete[]prev;
-}
-
-void shortestPath::Bellman_Ford(AdjencyMatrix& a, int source) {
-	std::vector<std::vector<int>> cost;
-	std::vector<std::vector<int>> prev;
-	cost.resize(a.nVer + 1);
-	prev.resize(a.nVer + 1);
-	for (int step = 0; step < a.nVer + 1; step++) {
-		cost[step].resize(a.nVer);
-		prev[step].resize(a.nVer);
-	}
-
-	// Khoi tao gia tri
-	for (int i = 0; i < a.nVer; i++) {
-		cost[0][i] = INT32_MAX;
-		prev[0][i] = -1;
-	}
-	cost[0][source] = 0;
-
-	// Run
-	for (int j = 0; j < a.nVer; j++) {
-		printf("%11d", cost[0][j]);
-	}
-	printf("\n");
-	int step = 1;
-	for (; step <= a.nVer; step++) {
-		for (int i = 0; i < a.nVer; i++) {
-			cost[step][i] = cost[step - 1][i];
-			prev[step][i] = prev[step - 1][i];
-		}
-		// Update cost[step][i]
-		for (int sr = 0; sr < a.nVer; sr++) {
-			// Neu da co duong di tu source den sr
-			if (cost[step - 1][sr] != INT32_MAX) {
-				for (int dst = 0; dst < a.nVer; dst++) {
-					if (a.data[sr][dst] != 0 &&
-						cost[step][dst] > (cost[step - 1][sr] + a.data[sr][dst])) {
-						cost[step][dst] = cost[step - 1][sr] + a.data[sr][dst];
-						prev[step][dst] = sr;
-					}
-				}
-			}
-		}
-		for (int j = 0; j < a.nVer; j++) {
-			printf("%11d", cost[step][j]);
-		}
-		printf("\n");
-
-		// Check 
-		int i = 0;
-		for (; i < a.nVer && cost[step][i] == cost[step - 1][i]; i++);
-		if (i == a.nVer) {
-			break;
-		}
-	}
-
-	if (step == (a.nVer + 1)) {
-		printf("Ton tai mach am trong do thi\n");
-	}
-	else {
-		for (int i = 0; i < a.nVer; i++) {
-			if (cost[step][i] != INT32_MAX && cost[step][i] != 0) {
-				printf("Duong di tu dinh %d den dinh %d: %d <- ", source + 1, i + 1, i + 1);
-				int ver = prev[step][i];
-				while (ver != source) {
-					printf("%d <- ", ver + 1);
-					ver = prev[step][ver];
-				}
-				printf("%d\n", ver + 1);
-			}
-		}
+		p = p->pNext;
 	}
 }
--- graph.h
+++ graph.h
@@ -4,18 +4,20 @@
 #include <vector>
 #include "linked_list.h"
 #include <limits.h>
-#include <list>
+
+typedef struct {
+	int u, v;
+	int u_pos, v_pos;
+} Edge;
 
 class AdjencyList {
 private:
-	int nVer;
 	std::vector<List*> lst;
+	int nVer;
 	friend class Euler;
 	friend class Hamilton;
 public:
-	// Function to parse input
-	void ParseLine(char* line, List*& lst);
-
+	friend void DFS_visit(AdjencyList* g, int ver, bool* label);
 	// Constructor & Destructor
 	AdjencyList(const char* filename);
 	~AdjencyList();
@@ -23,52 +25,74 @@ public:
 	// Copy constructor
 	AdjencyList(AdjencyList& org);
 
+	// Function to load graph from file
+	void LoadGraph(const char* filename);
+
+	// Print Graph
 	void printGraph();
+
+	// Define operator [] to access List[i]
+	List* operator[](int index);
 };
 
 class Euler {
+private:
 	AdjencyList* Graph;
+	int startVer;
+	int EulerType; // EulerType = 2 (circuit); = 1 (path); = 0 (none)
+	
 public:
-	Euler(AdjencyList* g) : Graph(g){}
-	void removeEdge(int u, int v);
-	void addEdge(int u, int v);
+	bool hasCircuit() const;
+	bool hasPath() const;
+	// Constructor
+	Euler(AdjencyList* g);
 
-	// Use DFS to check a graph is connected or not
-	void DFS_visit(int ver, bool* label);
+	// Operator with Edge
+	Edge removeEdge(int u, int v);
+	void addEdge(int u, int v, Edge e);
+
+	// Algorithm
 	bool isConnected();
-
-	// Euler trail/circuit
-	int isEuler(int startVer);
+	int checkEulerGraph(int startVer);
 	int DFS_Count(int ver, bool* label);
 	bool isBrigde(int u, int v);
+
+	// Print Result
 	void Euler_Rec(int ver);
 	void Euler_Print();
 };
 
 class Hamilton {
+private:
 	AdjencyList* Graph;
-	int* circuit;
+	int startVer;
+
+	// HamType = 2 (circuit); = 1 (path); = 0 (none)
+	int HamType;
+
+	// Store Circuit or Path of a Graph
+	std::vector<std::vector<int>> circuit;
+	std::vector<std::vector<int>> path;
+	std::vector<int> curPath;
 public:
+	bool hasCircuit() const;
+	bool hasPath() const;
 	// Constructor and Destructor
 	Hamilton(AdjencyList* g);
 	~Hamilton();
 
+	// Operator with Edge
+	Edge removeEdge(int u, int v);
+	void addEdge(int u, int v, Edge e);
 	bool isEdge(int u, int v);
-	void run(int ver);
-	void NextVertex(int ver);
+
+	// Algorithm
+	bool isConnected();
+	void run();
+	void Hamiton_Rec(int pos, int val);
+
+	// Result
+	void Ham_print();
 };
 
-class AdjencyMatrix {
-	int nVer;
-	std::vector<std::vector<int>> data;
-	friend class shortestPath;
-public:
-	AdjencyMatrix(const char* filename);
-	void printGraph();
-};
-
-class shortestPath {
-public: 
-	void Dijkstra(AdjencyMatrix& a, int source);
-	void Bellman_Ford(AdjencyMatrix& a, int source);
-};
+void DFS_visit(AdjencyList* g, int ver, bool* label);
